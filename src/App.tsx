6f80ed819492a13ef6b71f839c33b41174a03040
dev <dev@devs-MacBook-Pro.local>
2023-06-17 02:24:11 +0800

    Create jokes app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Button, CircularProgress, Container, Typography, Stack } from '@mui/material';
+import axios from 'axios';
 
-function App() {
+type Joke = {
+  joke: string;
+};
+
+const JokesApp: React.FC = () => {
+  const [jokes, setJokes] = useState<Joke[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchJokes = async () => {
+    setIsLoading(true);
+    try {
+      const responses = await Promise.all([
+        axios.get('https://v2.jokeapi.dev/joke/Programming?type=single'),
+        axios.get('https://v2.jokeapi.dev/joke/Programming?type=single'),
+        axios.get('https://v2.jokeapi.dev/joke/Programming?type=single'),
+      ]);
+      const newJokes: Joke[] = responses.map((response) => ({
+        joke: response.data.joke,
+      }));
+      newJokes.sort((a, b) => a.joke.length - b.joke.length);
+      setJokes(newJokes);
+    } catch (error) {
+      console.log('Error fetching joke:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchJokes();
+  }, []);
+
+  const refreshJokes = () => {
+    fetchJokes();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm" sx={{ marginTop: '2rem' }}>
+      <Stack spacing={2} sx={{ bgcolor: 'success.main', borderRadius: "10px" }}>
+      <Typography variant="h4" align="center" sx={{margin: "1rem auto", color: "white"}} gutterBottom>
+      Tell me a Joke
+      </Typography>
+      </Stack>
+      <Stack spacing={2}>
+      {isLoading ? (
+        <CircularProgress sx={{ display: 'block', margin: '7rem auto' }} />
+      ) : (
+        <ul>
+          {jokes.map((joke, index) => (
+            <Typography key={index} variant="body1" component="li" sx={{ marginBottom: '2rem', listStyleType: "none" }}>
+              {joke.joke}
+            </Typography>
+          ))}
+        </ul>
+      )}
+      </Stack>
+      
+      <Button variant="contained" onClick={refreshJokes} disabled={isLoading} sx={{borderRadius: "10px"}} fullWidth>
+        {isLoading ? 'Loading...' : 'Refresh'}
+      </Button>
+    </Container>
   );
-}
+};
 
-export default App;
+export default JokesApp;
